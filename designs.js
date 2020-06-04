e0f482043fe98078e84fdfd30b9d0fe72b29e961
Sarah B <smblair87@gmail.com>
2020-06-04 13:19:27 -0700

    Got basic logic functioning

--- designs.js
+++ designs.js
@@ -1,10 +1,37 @@
+// Global variables
+let canvas = document.getElementById('pixelCanvas');
+let height = document.getElementById('inputHeight');
+let width = document.getElementById('inputWidth');
+let sizePicker = document.getElementById('sizePicker');
+let color = document.getElementById('colorPicker');
+
 // Select color input
-// Select size input
+// Adds event listener when color is clicked on
+color.addEventListener('click', function() {});
 
-// When size is submitted by the user, call makeGrid()
+sizePicker.onsubmit = (event) => {
+  event.preventDefault();
+  clearGrid();
+  createGrid();
+};
 
-function makeGrid() {
+function createGrid() {
+  for (let i = 0; i < height.value; i++) {
+    const row = canvas.insertRow(i);
+    for (let j = 0; j < width.value; j++) {
+      const cell = row.insertCell(j);
+      cell.addEventListener('click', fillSquare);
+    }
+  }
+}
 
-// Your code goes here!
+function clearGrid() {
+  while (canvas.firstChild) {
+    canvas.removeChild(canvas.firstChild);
+  }
+}
 
+// Function to set background to selected color on canvas
+function fillSquare() {
+  this.setAttribute('style', `background-color: ${color.value}`);
 }
