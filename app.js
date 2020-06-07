5230d65c2cb5ea7907f99184fa35b16eeb4216d5
Sarah B <smblair87@gmail.com>
2020-06-07 12:27:39 -0700

    Working on feature to save popular colors to new array for each session

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ let sizePicker = document.getElementById('sizePicker');
 let eraseButton = document.getElementById('eraseButton');
 let color = document.getElementById('colorPicker');
 
+// Array to store most popular colors
+let popularColors = [];
+
 // Select color input
 // Adds event listener when color is clicked on
 color.addEventListener('click', function() {});
@@ -15,6 +18,9 @@ sizePicker.onsubmit = (event) => {
   clearGrid();
   createGrid();
 };
+
+let name = 'Sarah';
+console.log(name);
 
 const createGrid = () => {
   for (let i = 0; i < height.value; i++) {
@@ -35,6 +41,8 @@ const clearGrid = () => {
 // Function to set background to selected color on canvas
 function fillSquare() {
   this.setAttribute('style', `background-color: ${color.value}`);
+  popularColors.push(`${color.value}`);
+  console.log(popularColors);
 }
 
 // Function to convert canvas to an image
